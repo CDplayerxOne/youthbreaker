78c00c781cda9ca3efe004f761f2b59eed5ffdff
Corey Dai <73034147+CDplayerxOne@users.noreply.github.com>
2023-11-08 23:55:53 -0500

    deploy

--- utils/socket.ts
+++ utils/socket.ts
@@ -2,6 +2,8 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-  process.env.NODE_ENV === "production" ? undefined : "http://localhost:6135";
+  process.env.NODE_ENV === "production"
+    ? process.env.NEXT_PUBLIC_SERVER_URL
+    : "http://localhost:6135";
 
 export const socket = io(URL as string);
